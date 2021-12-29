bfb5a5a969e77885e03a3b21ad23e3308b378ac2
DrinkH20 <89287648+DrinkH20@users.noreply.github.com>
2021-12-29 11:49:58 -0800

    pretty much done now

--- Main.py
+++ Main.py
@@ -6,10 +6,16 @@ pieces = []
 white_pieces = [(50, 100), (150, 100), (250, 100), (350, 100),
                 (0, 50), (100, 50), (200, 50), (300, 50),
                 (50, 0), (150, 0), (250, 0), (350, 0)]
+white_kings = [0, 0, 0, 0,
+               0, 0, 0, 0,
+               0, 0, 0, 0]
 
 black_pieces = [(0, 250), (100, 250),  (200, 250), (300, 250),
                 (50, 300), (150, 300), (250, 300), (350, 300),
                 (0, 350), (100, 350),  (200, 350), (300, 350)]
+black_kings = [0, 0, 0, 0,
+               0, 0, 0, 0,
+               0, 0, 0, 0]
 Dist = 0
 mx, my = pygame.mouse.get_pos()
 run = True
@@ -43,20 +49,37 @@ def move(re1, re2, item):
         part1 = 0
         dist = white_pieces[item]
         # This part is for jumping over a black player
-        if abs(dist[0] - re1) == 100:
-            pygame.display.set_caption("Red's turn")
-            # This is where it keeps it our turn and tells that we've jumped over someone
-            turn = '1'
-            jumped = 1
-            # This is where is deletes the jumped player
-            replace = black_pieces.index((re1 + (dist[0] - re1)/2, re2 + (dist[1] - re2)/2))
-            black_pieces.remove(black_pieces[replace])
-            current_piece = [(), (), (), "1"]
+        calc = dist[1] - re2
+        if abs(calc) == 100:
+            if white_kings[item] == 0:
+                if calc == -100:
+                    pygame.display.set_caption("Red's turn")
+                    # This is where it keeps it our turn and tells that we've jumped over someone
+                    turn = '1'
+                    jumped = 1
+                    # This is where is deletes the jumped player
+                    replace = black_pieces.index((re1 + (dist[0] - re1)/2, re2 + (dist[1] - re2)/2))
+                    black_pieces.remove(black_pieces[replace])
+                    current_piece = [(), (), (), "1"]
 
-            # This is where it moves the player
-            white_pieces[item] = re1, re2
-            opposite_team_pieces = black_pieces
-            current_team = white_pieces
+                    # This is where it moves the player
+                    white_pieces[item] = re1, re2
+                    opposite_team_pieces = black_pieces
+                    current_team = white_pieces
+            else:
+                pygame.display.set_caption("Red's turn")
+                # This is where it keeps it our turn and tells that we've jumped over someone
+                turn = '1'
+                jumped = 1
+                # This is where is deletes the jumped player
+                replace = black_pieces.index((re1 + (dist[0] - re1) / 2, re2 + (dist[1] - re2) / 2))
+                black_pieces.remove(black_pieces[replace])
+                current_piece = [(), (), (), "1"]
+
+                # This is where it moves the player
+                white_pieces[item] = re1, re2
+                opposite_team_pieces = black_pieces
+                current_team = white_pieces
 
         #     Notice: I didn't reset the current_player, this is because we want to keep are player's turn going
         else:
@@ -64,12 +87,22 @@ def move(re1, re2, item):
 
             # This is for moving 1 space at the beginning of ur turn
             if jumped == 0:
-                # This is saying that we didn't jump over someone and that its the black's turn
-                jumped = 0
-                turn = '-1'
-                # Making it so that there is no piece selected to move
-                current_piece = [(), (), (), ""]
-                white_pieces[item] = re1, re2
+                if white_kings[item] == 0:
+                    if calc == -50:
+                        # This is saying that we didn't jump over someone and that its the black's turn
+                        jumped = 0
+                        turn = '-1'
+                        # Making it so that there is no piece selected to move
+                        current_piece = [(), (), (), ""]
+                        white_pieces[item] = re1, re2
+
+                else:
+                    # This is saying that we didn't jump over someone and that its the black's turn
+                    jumped = 0
+                    turn = '-1'
+                    # Making it so that there is no piece selected to move
+                    current_piece = [(), (), (), ""]
+                    white_pieces[item] = re1, re2
 
             # This is for moving when 1 space after jumping, so u can't move 1 space :P
             else:
@@ -79,29 +112,52 @@ def move(re1, re2, item):
                 pygame.display.set_caption("Black's turn")
             opposite_team_pieces = white_pieces
             current_team = black_pieces
+        if re2 == 350:
+            white_kings[item] = 1
 
     elif turn == '-1':
         part2 = 0
         dist = black_pieces[item]
-        if abs(dist[0] - re1) == 100:
-            jumped = 1
-            pygame.display.set_caption("Black's turn")
-            turn = '-1'
-            replace = white_pieces.index((re1 + (dist[0] - re1) / 2, re2 + (dist[1] - re2) / 2))
-            white_pieces.remove(white_pieces[replace])
-            black_pieces[item] = re1, re2
-            current_piece = [(), (), (), "-1"]
-            opposite_team_pieces = white_pieces
-            current_team = black_pieces
+        calc = dist[1] - re2
+        if abs(calc) == 100:
+            if black_kings[item] == 0:
+                if calc == 100:
+                    jumped = 1
+                    pygame.display.set_caption("Black's turn")
+                    turn = '-1'
+                    replace = white_pieces.index((re1 + (dist[0] - re1) / 2, re2 + (dist[1] - re2) / 2))
+                    white_pieces.remove(white_pieces[replace])
+                    black_pieces[item] = re1, re2
+                    current_piece = [(), (), (), "-1"]
+                    opposite_team_pieces = white_pieces
+                    current_team = black_pieces
+            else:
+                jumped = 1
+                pygame.display.set_caption("Black's turn")
+                turn = '-1'
+                replace = white_pieces.index((re1 + (dist[0] - re1) / 2, re2 + (dist[1] - re2) / 2))
+                white_pieces.remove(white_pieces[replace])
+                black_pieces[item] = re1, re2
+                current_piece = [(), (), (), "-1"]
+                opposite_team_pieces = white_pieces
+                current_team = black_pieces
 
         else:
             if jumped == 0:
-                jumped = 0
-                turn = '1'
-                current_piece = [(), (), (), ""]
-                pygame.display.set_caption("Red's turn")
-                black_pieces[item] = re1, re2
-
+                if abs(calc) == 50:
+                    if black_kings[item] == 0:
+                        if calc == 50:
+                            jumped = 0
+                            turn = '1'
+                            current_piece = [(), (), (), ""]
+                            pygame.display.set_caption("Red's turn")
+                            black_pieces[item] = re1, re2
+                    else:
+                        jumped = 0
+                        turn = '1'
+                        current_piece = [(), (), (), ""]
+                        pygame.display.set_caption("Red's turn")
+                        black_pieces[item] = re1, re2
             else:
                 jumped = 0
                 turn = '1'
@@ -110,6 +166,8 @@ def move(re1, re2, item):
             opposite_team_pieces = black_pieces
             current_team = white_pieces
         part = 0
+        if re2 == 0:
+            black_kings[item] = 1
 
 
 def can_move(place, moves):
@@ -186,7 +244,11 @@ def screen_draw(w, h, re=0, repeat=0, repeat2=0):
                 if current_piece[1] == (repeat, repeat2):
                     pygame.draw.rect(WIN, (250, 80, 80), (repeat + 50, repeat2 + 50, 45, 45))
                 else:
-                    pygame.draw.rect(WIN, (250, 50, 50), (repeat + 50, repeat2 + 50, 45, 45))
+                    # This makes the color of the kings and the regular players different
+                    if white_kings[white_pieces.index((repeat, repeat2))] == 1:
+                        pygame.draw.rect(WIN, (180, 50, 50), (repeat + 50, repeat2 + 50, 45, 45))
+                    else:
+                        pygame.draw.rect(WIN, (250, 50, 50), (repeat + 50, repeat2 + 50, 45, 45))
 
                 if (50 * round(int(mx - 25)/50)) == repeat + 50 and (50 * round(int(my - 25)/50)) == repeat2 + 50:
                     if click[0] and turn == '1':
@@ -195,7 +257,10 @@ def screen_draw(w, h, re=0, repeat=0, repeat2=0):
                 if current_piece[1] == (repeat, repeat2):
                     pygame.draw.rect(WIN, (20, 20, 20), (repeat + 50, repeat2 + 50, 45, 45))
                 else:
-                    pygame.draw.rect(WIN, (30, 30, 30), (repeat + 50, repeat2 + 50, 45, 45))
+                    if black_kings[black_pieces.index((repeat, repeat2))] == 1:
+                        pygame.draw.rect(WIN, (15, 15, 15), (repeat + 50, repeat2 + 50, 45, 45))
+                    else:
+                        pygame.draw.rect(WIN, (30, 30, 30), (repeat + 50, repeat2 + 50, 45, 45))
 
                 if (50 * round(int(mx - 25)/50)) == repeat + 50 and (50 * round(int(my - 25)/50)) == repeat2 + 50:
                     if click[0] and turn == '-1':
